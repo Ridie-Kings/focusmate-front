941c7434593bbde9dfecf1d1a260e896e1d88844
Ridie-Kings <fran6salvatierra@gmail.com>
2025-02-12 17:45:49 +0100

    feat: creation of api module & login and register logic adapted to the app

--- services/auth/login.ts
+++ services/auth/login.ts
@@ -1,14 +1,51 @@
-import { LoginProps } from "@/interfaces/Auth/AuthType";
+"use server";
 import { apiConnection } from "../axiosConfig";
+import { cookies } from "next/headers";
 
-export async function login({ email, password }: LoginProps) {
+export async function login(prevState: any, formData: FormData) {
   try {
-    const res = await apiConnection.post("auth/login", { email, password });
-    return res.data;
+    const existedCookies = await cookies();
+    const userData = {
+      email: formData.get("email"),
+      password: formData.get("password"),
+    };
+
+    for (const key of Object.keys(userData) as (keyof typeof userData)[]) {
+      if (userData[key] === "") {
+        return {
+          success: false,
+          message: `${userData[key]} field must be filled`,
+        };
+      }
+    }
+    const res = await apiConnection.post("auth/login", userData);
+    const accessToken = res.data.accessToken;
+    const refreshToken = res.data.refreshToken;
+    const softExpired = new Date(Date.now() + 1000 * 60 * 60 * 12);
+    const hardExpired = new Date(Date.now() + 1000 * 60 * 60 * 24 * 7);
+    existedCookies.set("token", accessToken, {
+      expires: softExpired,
+      httpOnly: true,
+    });
+    existedCookies.set("session", refreshToken, {
+      expires: hardExpired,
+      httpOnly: true,
+    });
+
+    return {
+      success: true,
+      message: res.data,
+    };
   } catch (error: Error | any) {
     if (error.response) {
-      throw new Error(error.response.data.error || "Failed to login");
+      return {
+        success: false,
+        message: error.response.data.error || "Failed to register",
+      };
     }
-    throw new Error("Failed to login");
+    return {
+      success: false,
+      message: "Unexpected error through register",
+    };
   }
 }
--- services/auth/register.ts
+++ services/auth/register.ts
@@ -1,24 +1,37 @@
-import { RegisterProps } from "@/interfaces/Auth/AuthType";
 import { apiConnection } from "../axiosConfig";
 
-export async function register({
-  fullName,
-  username,
-  email,
-  password,
-}: RegisterProps) {
+export async function register(prevState: any, formData: FormData) {
   try {
-    const res = await apiConnection.post("auth/register", {
-      fullName,
-      username,
-      email,
-      password,
-    });
-    return res.data;
+    const userData = {
+      fullname: formData.get("fullname"),
+      username: formData.get("username"),
+      email: formData.get("email"),
+      password: formData.get("password"),
+    };
+
+    for (const key of Object.keys(userData) as (keyof typeof userData)[]) {
+      if (userData[key] === "") {
+        return {
+          success: false,
+          message: `${userData[key]} field must be filled`,
+        };
+      }
+    }
+    const res = await apiConnection.post("auth/register", userData);
+    return {
+      success: true,
+      message: res.data,
+    };
   } catch (error: Error | any) {
     if (error.response) {
-      throw new Error(error.response.data.error || "Failed to register");
+      return {
+        success: false,
+        message: error.response.data.error || "Failed to register",
+      };
     }
-    throw new Error("Failed to register");
+    return {
+      success: false,
+      message: "Unexpected error through register",
+    };
   }
 }
