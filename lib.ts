2d1239f03b068f220b141268c84a8137f816fb4f
albocoq <albocoq@gmail.com>
2025-02-21 11:55:35 +0100

    fixed merge lib

--- lib.ts
+++ lib.ts
@@ -18,41 +18,24 @@ export async function updateSession(req: NextRequest) {
   const existedCookies = await cookies();
   const accessToken = await getToken();
   const refreshToken = existedCookies.get("session");
-<<<<<<< HEAD
   const { pathname } = req.nextUrl;
   const publicPaths = new Set(["/login", "/register"]);
 
   if (!accessToken && refreshToken) {
     await refreshSession(refreshToken);
   }
-	const existedCookies = await cookies();
-	const accessToken = await getToken();
-	const refreshToken = existedCookies.get("session");
-	const url = req.url;
-	const { pathname } = req.nextUrl;
-	const publicPaths = new Set(["/login", "/register"]);
-=======
-  const url = req.url;
-  const { pathname } = req.nextUrl;
-  const publicPaths = new Set(["/login", "/register"]);
->>>>>>> ff62aa5 (fixed infinite redirect)
 
   if (!accessToken && refreshToken) await refreshSession(refreshToken);
 
   if (!pathname.includes(".")) {
     if (!accessToken && !refreshToken && !publicPaths.has(pathname)) {
-<<<<<<< HEAD
       return NextResponse.redirect(new URL("/login", req.nextUrl.origin));
     }
   }
-	if (!pathname.includes(".")) {
-		if (!accessToken && !refreshToken && !publicPaths.has(pathname)) {
-			// return NextResponse.redirect(new URL("/login", req.nextUrl.origin));
-		}
-=======
+  if (!pathname.includes(".")) {
+    if (!accessToken && !refreshToken && !publicPaths.has(pathname)) {
       // return NextResponse.redirect(new URL("/login", req.nextUrl.origin));
     }
->>>>>>> ff62aa5 (fixed infinite redirect)
 
     if (accessToken && refreshToken && publicPaths.has(pathname)) {
       // return NextResponse.redirect(new URL("/", req.nextUrl.origin));
@@ -61,7 +44,6 @@ export async function updateSession(req: NextRequest) {
 }
 
 export async function refreshSession(refreshToken: RequestCookie | undefined) {
-<<<<<<< HEAD
   if (!refreshToken) return;
 
   try {
@@ -71,7 +53,13 @@ export async function refreshSession(refreshToken: RequestCookie | undefined) {
     const newAccessToken = res.data.accessToken;
 
     const existedCookies = await cookies();
-=======
+    existedCookies.set("token", newAccessToken, {
+      expires: new Date(Date.now() + 1000 * 60 * 60 * 12),
+      httpOnly: true,
+    });
+  } catch (error) {
+    console.error("Error refreshing session:", error);
+  }
   try {
     const existedCookies = await cookies();
     const res = await apiConnection.post("auth/refresh", { refreshToken });
@@ -80,49 +68,17 @@ export async function refreshSession(refreshToken: RequestCookie | undefined) {
       console.error("Failed to refresh session: No access token returned");
       return undefined;
     }
->>>>>>> ff62aa5 (fixed infinite redirect)
     existedCookies.set("token", newAccessToken, {
       expires: new Date(Date.now() + 1000 * 60 * 60 * 12),
       httpOnly: true,
     });
-<<<<<<< HEAD
-  } catch (error) {
-    console.error("Error refreshing session:", error);
-  }
-	try {
-		const existedCookies = await cookies();
-		const res = await apiConnection.post("auth/refresh", { refreshToken });
-		const newAccessToken = res.data.accessToken;
-		if (!newAccessToken) {
-			console.error(
-				"Failed to refresh session: No access token returned"
-			);
-			return undefined;
-		}
-		existedCookies.set("token", newAccessToken, {
-			expires: new Date(Date.now() + 1000 * 60 * 60 * 12),
-			httpOnly: true,
-		});
-		return newAccessToken;
-	} catch (error) {
-		console.error("Error refreshing session:", error);
-		return undefined;
-	}
-=======
     return newAccessToken;
   } catch (error) {
     console.error("Error refreshing session:", error);
     return undefined;
   }
->>>>>>> ff62aa5 (fixed infinite redirect)
 }
 
-export async function logout() {
-  const existedCookies = await cookies();
-  existedCookies.set("token", "", { expires: new Date(0) });
-  existedCookies.set("session", "", { expires: new Date(0) });
-
-  return NextResponse.redirect("/login");
 export async function logout(req: NextRequest) {
   const existedCookies = await cookies();
   const url = req.url;
