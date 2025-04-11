fd5461863ee7ffa32b84c644d13276fc336893ad
cllopez <claudiolopero@gmail.com>
2025-04-11 19:44:11 +0200

    refactor: remove logging of API key in send-contact route

--- app/api/send-contact/route.ts
+++ app/api/send-contact/route.ts
@@ -2,7 +2,6 @@ import { Resend } from "resend";
 import { NextRequest, NextResponse } from "next/server";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
-console.log("API KEY de RESEND:", process.env.RESEND_API_KEY);
 
 
 export async function POST(req: NextRequest) {
