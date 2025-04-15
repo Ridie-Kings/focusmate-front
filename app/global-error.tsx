c44cd3c5191cb887b6ed842a88f012941904eac5
albocoq <albocoq@gmail.com>
2025-04-15 18:52:11 +0200

    fix: improve token removal flow and enhance image component formatting in GlobalError

--- app/global-error.tsx
+++ app/global-error.tsx
@@ -1,13 +1,17 @@
 "use client";
 import { logout } from "@/lib";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 export default function GlobalError() {
+  const router = useRouter();
+
   useEffect(() => {
     const handleRemoveTokens = async () => {
       setTimeout(async () => {
         await logout();
+        router.push("/login");
       }, 1000);
     };
     handleRemoveTokens();
@@ -16,7 +20,13 @@ export default function GlobalError() {
   return (
     <html>
       <body className="flex items-center justify-center w-screen h-screen">
-        <Image src={"/images/tenor.gif"} width={480} height={480} alt="error" />
+        <Image
+          src={"/images/tenor.gif"}
+          width={480}
+          height={480}
+          alt="error"
+          className="mx-auto"
+        />
       </body>
     </html>
   );
