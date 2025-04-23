7ffb4e12979c0c8b47edd638f6ba086446beb20e
albocoq <albocoq@gmail.com>
2025-04-23 19:47:43 +0200

    fix: correct Spanish verb form in FreeMembership component; update focus time in TimerProvider

--- components/Elements/BannerMembership/BannersTypes/FreeMembership.tsx
+++ components/Elements/BannerMembership/BannersTypes/FreeMembership.tsx
@@ -7,7 +7,7 @@ export default function FreeMembership() {
       <div className="absolute w-3/5 px-8 flex flex-col gap-2 z-50">
         <p className="text-xl text-black">Membresía Gratuita</p>
         <p className="text-sm text-gray-400">
-          Explorá lo esencial. Accede a funciones básicas para organizar tus
+          Explora lo esencial. Accede a funciones básicas para organizar tus
           hábitos y avanzar a tu ritmo. Ideal para comenzar tu camino sin
           límites.
         </p>
