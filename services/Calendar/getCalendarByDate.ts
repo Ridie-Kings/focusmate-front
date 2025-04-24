a19b1b04ccd5c9156bb38f403f69eda222ec6d29
albocoq <albocoq@gmail.com>
2025-04-24 22:47:10 +0200

    refactor: simplify date formatting in getCalendarByDate function

--- services/Calendar/getCalendarByDate.ts
+++ services/Calendar/getCalendarByDate.ts
@@ -9,11 +9,11 @@ export async function getCalendarByDate({ date }: { date: Date }): Promise<{
   try {
     const token = await getToken();
 
-    const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, "0");
-    const day = String(date.getDate()).padStart(2, "0");
+    // const year = date.getFullYear();
+    // const month = String(date.getMonth() + 1).padStart(2, "0");
+    // const day = String(date.getDate()).padStart(2, "0");
 
-    const formattedDate = `${year}-${month}-${day}`;
+    const formattedDate = date.toISOString().split("T")[0];
 
     const res = await apiConnection.get(`calendar/${formattedDate}`, {
       headers: {
