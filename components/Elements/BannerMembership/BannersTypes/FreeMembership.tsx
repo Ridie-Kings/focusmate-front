2c098e84e7dce63e57661fd93cbee51a8515245f
albocoq <albocoq@gmail.com>
2025-05-23 13:00:45 +0200

    feat: enhance membership banners and profile layout with responsive adjustments

--- components/Elements/BannerMembership/BannersTypes/FreeMembership.tsx
+++ components/Elements/BannerMembership/BannersTypes/FreeMembership.tsx
@@ -4,9 +4,9 @@ export default function FreeMembership() {
   return (
     <>
       <FreeMembershipSvg />
-      <div className="absolute w-3/5 px-8 flex flex-col gap-2 z-50">
+      <div className="absolute w-full sm:w-3/5 px-8 flex flex-col gap-2 z-50">
         <p className="text-xl text-black">Membresía Gratuita</p>
-        <p className="text-sm text-gray-400">
+        <p className="text-sm text-gray-400 line-clamp-3">
           Explora lo esencial. Accede a funciones básicas para organizar tus
           hábitos y avanzar a tu ritmo. Ideal para comenzar tu camino sin
           límites.
--- components/Pages/(Nav)/Profile/InfoProfile.tsx
+++ components/Pages/(Nav)/Profile/InfoProfile.tsx
@@ -8,7 +8,7 @@ export default function InfoProfile({
   profile: ProfileType | null;
 }) {
   return (
-    <div className="flex gap-6 flex-1">
+    <div className="flex flex-col lg:flex-row gap-6 flex-1">
       <DescriptionProfile profile={profile} />
       <Acheivements />
     </div>
